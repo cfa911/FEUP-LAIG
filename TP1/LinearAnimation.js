8f675374fe08b0ba9b29327980729ede72d1b320
mcarolinaSoares <up201605010@fe.up.pt>
2018-11-12 14:52:19 +0000

    animations refactoring

--- TP1/LinearAnimation.js
+++ TP1/LinearAnimation.js
@@ -9,16 +9,16 @@ class LinearAnimation extends Animation{
         // duraçao do movimento
         this.time = time;
         // distancia calculada em cada reta (p2-p1) (p3-p2) (p3-p1)
-        var vecCPoints = [];
+        this.vecCPoints = [];
         // distancia total de todas as retas
-        var distance = 0;
+        this.distance = 0;
     }
 
     calculateDistance() {
 
         for(var i=0; i<=this.controlPts.length; i++) {
-            distance += Math.sqrt(Math.pow(this.controlPts[i+1][0] - this.controlPts[i][0], 2) + Math.pow(this.controlPts[i+1][1] - this.controlPts[i][1], 2) + Math.pow(this.controlPts[i+1][2] - this.controlPts[i][2], 2));
-            vecCPoints.push(Math.sqrt(Math.pow(this.controlPts[i+1][0] - this.controlPts[i][0], 2) + Math.pow(this.controlPts[i+1][1] - this.controlPts[i][1], 2) + Math.pow(this.controlPts[i+1][2] - this.controlPts[i][2], 2)));
+            this.distance += Math.sqrt(Math.pow(this.controlPts[i+1][0] - this.controlPts[i][0], 2) + Math.pow(this.controlPts[i+1][1] - this.controlPts[i][1], 2) + Math.pow(this.controlPts[i+1][2] - this.controlPts[i][2], 2));
+            this.vecCPoints.push(Math.sqrt(Math.pow(this.controlPts[i+1][0] - this.controlPts[i][0], 2) + Math.pow(this.controlPts[i+1][1] - this.controlPts[i][1], 2) + Math.pow(this.controlPts[i+1][2] - this.controlPts[i][2], 2)));
         }
     }
 

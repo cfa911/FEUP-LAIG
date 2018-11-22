41a5fc5d1a1c64de337a8b7bb7c4697b74191355
Christopher Abreu <cfa911@outlook.pt>
2018-11-22 19:45:26 +0000

    Plane

--- TP2/MyPlane.js
+++ TP2/MyPlane.js
@@ -6,25 +6,37 @@
 
 class MyPlane extends CGFobject
 {
-	constructor(scene, npartsU, npartsV,minS = 0, maxS = 1, minT = 0, maxT = 1)
+	constructor(scene, npartsU, npartsV,degree1 = 1,degree2 = 1,controlvertexes = [[[-1.0, -1.0, 0.0, 0.5 ],[-1.0,  1.0, 0.0, 0.5 ]],[[ 1.0, -1.0, 0.0,0.5 ],[ 1.0, 1.0, 0.0, 0.5 ]]],translation = [0,0,0])
 	{
         super(scene);
         this.npartsU = npartsU;
         this.npartsV = npartsV;
-
-		this.minS = minS;
-        this.maxS = maxS;
-        this.minT = minT;
-        this.maxT = maxT;
-
-		this.initBuffers();
+        this.degree1 = degree1;
+        this.degree2 = degree2;
+        this.controlvertexes = controlvertexes;
+        this.translation = translation;
+        this.surfaces = [];
+        this.translations = [];
+        this.create();
 	};
 
-	initBuffers()
+	create()
 	{
-
+		var nurbsSurface = new CGFnurbsSurface(this.degree1 , this.degree2, this.controlvertexes);
+		var obj = new CGFnurbsObject(this.scene,  this.npartsU,  this.npartsV, nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
+		this.surfaces.push(obj);	
+        this.translations.push(this.translation);
     }
-    
+    display(){
+        for (i =0; i<this.surfaces.length; i++) {
+			this.scene.pushMatrix();
+		
+			this.scene.translate(this.translations[i][0], this.translations[i][1], this.translations[i][2]);
+
+			this.surfaces[i].display();
+			this.scene.popMatrix();
+		}
+    }
     changeLength(length_s, length_t) {/*
 
         this.length_s = length_s;

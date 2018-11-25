c9af51177b168626e45960ad0c4af5ddc403592b
Christopher Abreu <cfa911@outlook.pt>
2018-11-25 01:33:31 +0000

    commit errors sorted

--- TP2/MyPlane.js
+++ TP2/MyPlane.js
@@ -6,7 +6,7 @@
 
 class MyPlane extends CGFobject
 {
-	constructor(scene, npartsU, npartsV, degree1 = 1, degree2 = 1, controlvertexes = [[[-0.5, -0.5, 0.0, 1], [-0.5,  0.5, 0.0, 1]], [[0.5, -0.5, 0.0, 1], [0.5, 0.5, 0.0, 1]]], translation = [0,0,0])
+	constructor(scene, npartsU, npartsV, degree1 = 1, degree2 = 1, controlvertexes = [[[0.5, 0.0, 0.5, 1], [-0.5,  0.0, 0.5, 1]], [[0.5, 0, -0.5, 1], [-0.5, 0.0, -0.5, 1]]], translation = [0,0,0])
 	{
         super(scene);
         this.npartsU = npartsU;
@@ -21,30 +21,13 @@ class MyPlane extends CGFobject
 	create()
 	{
 		var nurbsSurface = new CGFnurbsSurface(this.degree1 , this.degree2, this.controlvertexes);
-		var obj = new CGFnurbsObject(this.scene, this.npartsU,  this.npartsV, nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
-		this.surfaces = obj;
+		this.surfaces = new CGFnurbsObject(this.scene, this.npartsU,  this.npartsV, nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
     }
     display(){
 			this.scene.pushMatrix();
-            this.scene.rotate(270*DEGREE_TO_RAD,1,0,0);
 			this.scene.translate(this.translation[0], this.translation[1], this.translation[2]);
-			this.surfaces.display();
+            this.surfaces.display();
 			this.scene.popMatrix();
 		
-    }
-    changeLength(length_s, length_t) {/*
-
-        this.length_s = length_s;
-		this.length_t = length_t;
-    
-        for(var i = 0; i <= this.stacks; i++)
-        {
-            for(var j = 0; j <= this.slices; j++)
-            {
-               this.texCoords.push(j * length_s / this.slices, i * length_t / this.stacks);
-            }    
-        }
-
-        this.updateTexCoordsGLBuffers();*/
     }
 };
